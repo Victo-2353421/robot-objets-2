45e30608cc34410bc31ed6c19151af381abe6555
MorganBois <2353421@carrefour.cegepvicto.ca>
2026-04-27 10:37:59 -0400

    roues fonctionnelles

--- robot-objets-2.ino
+++ robot-objets-2.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoBLE.h>
 
 
 // numéro de pin.
@@ -62,16 +63,16 @@ constexpr Pin ROUE_ARRIERE_GAUCHE_AVANCER = A6;
 constexpr Pin ROUE_ARRIERE_GAUCHE_RECULER = A7;
 constexpr Pin ROUE_ARRIERE_GAUCHE_PWM = 3;
 //AVANT_GAUCHE
-constexpr Pin ROUE_AVANT_GAUCHE_AVANCER = 0; // D0/RX
-constexpr Pin ROUE_AVANT_GAUCHE_RECULER = 1; // D1/TX
+constexpr Pin ROUE_AVANT_GAUCHE_AVANCER = 1; //normalement 0? // D0/RX
+constexpr Pin ROUE_AVANT_GAUCHE_RECULER = 0; //normalement 1? // D1/TX
 constexpr Pin ROUE_AVANT_GAUCHE_PWM = 2;
 //ARRIERE_DROITE
 constexpr Pin ROUE_ARRIERE_DROITE_AVANCER = 5;
 constexpr Pin ROUE_ARRIERE_DROITE_RECULER = 4;
 constexpr Pin ROUE_ARRIERE_DROITE_PWM = 6;
 //AVANT_DROITE
-constexpr Pin ROUE_AVANT_DROITE_AVANCER = 7;
-constexpr Pin ROUE_AVANT_DROITE_RECULER = 8;
+constexpr Pin ROUE_AVANT_DROITE_AVANCER = 8; // normalement 7?
+constexpr Pin ROUE_AVANT_DROITE_RECULER = 7; // normalement 8?
 constexpr Pin ROUE_AVANT_DROITE_PWM = 9;
 
 // mapper entre 80 à 180
@@ -96,14 +97,31 @@ struct Actions {
     /**
      * Lire l'état de la manette et retourner son Actions correspondant.
      */
-    static Actions lire(US deltaTime){
-        return Actions{};
+    //static Actions lire(US deltaTime){
+    //    return Actions{};
+    //}
+
+    constexpr Actions(int8_t avant, int8_t lacet, int8_t lateral)
+      : avant(avant)
+      , lacet(lacet)
+      , lateral(lateral)
+    {
+
     }
 
     /**
      * Affiche la struct dans le moniteur de série
      */
-    void print() const{}
+    void print() const{
+        Serial.print("avant=");
+        Serial.print(avant);
+        
+        Serial.print(" lacet=");
+        Serial.print(lacet);
+        
+        Serial.print(" lateral=");
+        Serial.println(lateral);
+    }
 };
 
 
@@ -113,43 +131,80 @@ void gererMouvement(struct Actions actions)
     const int16_t lacet = actions.lacet;
     const int16_t lateral = actions.lateral;
 
-    const int8_t min = ROUE_VITESSE_MIN;
-    const int8_t max = ROUE_VITESSE_MAX;
+    const int8_t min = -127;
+    const int8_t max = 127;
     int8_t avantGauche = util::conversionClamp(avant - lacet - lateral, min, max);
     int8_t arriereGauche = util::conversionClamp(avant - lacet + lateral, min, max);
     int8_t avantDroite = util::conversionClamp(avant + lacet + lateral, min, max);
     int8_t arriereDroite = util::conversionClamp(avant + lacet - lateral, min, max);
 
-    const auto gererOutput = [](int8_t valeur, Pin avancerPin, Pin reculerPin, Pin pwmPin) {
-        if(valeur < 0) {
-            digitalWrite(avancerPin, LOW);
-            digitalWrite(reculerPin, HIGH);
-        } else if(valeur > 0) {
-            digitalWrite(avancerPin, HIGH);
-            digitalWrite(reculerPin, LOW);
-        } else {
-            digitalWrite(avancerPin, LOW);
-            digitalWrite(reculerPin, LOW);
-        }
+    const auto gererOutput = [](int8_t valeur, Pin avancerPin, Pin reculerPin, Pin pwmPin, const char *dbgNom) {
+        bool avancerBool;
+        bool reculerBool;
 
-        const auto mapperVitesse = [](int8_t vitesse){
+        if(valeur < 0) {
+            avancerBool = LOW;
+            reculerBool = HIGH;
+        } else if(valeur > 0) {
+            avancerBool = HIGH;
+            reculerBool = LOW;
+        } else {
+            avancerBool = LOW;
+            reculerBool = LOW;
+        }
+        digitalWrite(avancerPin, avancerBool);
+        digitalWrite(reculerPin, reculerBool);
+
+        const auto mapperVitesse = [](int8_t vitesse) -> uint8_t {
             uint8_t vitessePositive = abs(vitesse);
-            if(vitessePositive == 128) vitessePositive = 127;
-            //if(vitessePositive == 0) return 0;
+            if(vitessePositive == 0) return 0;
             return util::map<uint8_t>(vitessePositive, 0, 127, ROUE_VITESSE_MIN, ROUE_VITESSE_MAX);
         };
 
-        analogWrite(pwmPin, mapperVitesse(valeur));
+        const auto outputPwm = mapperVitesse(valeur);
+        analogWrite(pwmPin, outputPwm);
+
+/*
+        Serial.print(dbgNom);
+        Serial.println(":");
+        Serial.print(" avancerPin : ");
+        Serial.println(avancerBool);
+        Serial.print(" reculerPin : ");
+        Serial.println(reculerBool);
+        Serial.print(" pwmPin : ");
+        Serial.println(outputPwm);
+        Serial.println("");
+        */
     };
 
-    gererOutput(avantGauche, ROUE_AVANT_GAUCHE_AVANCER, ROUE_AVANT_GAUCHE_RECULER, ROUE_AVANT_GAUCHE_PWM);
-    gererOutput(arriereGauche, ROUE_ARRIERE_GAUCHE_AVANCER, ROUE_ARRIERE_GAUCHE_RECULER, ROUE_ARRIERE_GAUCHE_PWM);
-    gererOutput(avantDroite, ROUE_AVANT_DROITE_AVANCER, ROUE_AVANT_DROITE_RECULER, ROUE_AVANT_DROITE_PWM);
-    gererOutput(arriereDroite, ROUE_ARRIERE_DROITE_AVANCER, ROUE_ARRIERE_DROITE_RECULER, ROUE_ARRIERE_DROITE_PWM);
+    gererOutput(avantGauche, ROUE_AVANT_GAUCHE_AVANCER, ROUE_AVANT_GAUCHE_RECULER, ROUE_AVANT_GAUCHE_PWM, "avantGauche");
+    gererOutput(arriereGauche, ROUE_ARRIERE_GAUCHE_AVANCER, ROUE_ARRIERE_GAUCHE_RECULER, ROUE_ARRIERE_GAUCHE_PWM, "arriereGauche");
+    gererOutput(avantDroite, ROUE_AVANT_DROITE_AVANCER, ROUE_AVANT_DROITE_RECULER, ROUE_AVANT_DROITE_PWM, "avantDroite");
+    gererOutput(arriereDroite, ROUE_ARRIERE_DROITE_AVANCER, ROUE_ARRIERE_DROITE_RECULER, ROUE_ARRIERE_DROITE_PWM, "arriereDroite");
 }
 
 
+
+BLEService nusService("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+BLECharacteristic rxChar("6E400002-B5A3-F393-E0A9-E50E24DCCA9E", BLEWrite | BLEWriteWithoutResponse, 20);
+BLECharacteristic txChar("6E400003-B5A3-F393-E0A9-E50E24DCCA9E", BLENotify, 20);
+
 void setup() {
+    Serial.begin(9600);
+    while (!Serial);
+
+    { // Setup BLE
+        BLE.begin();
+        BLE.setLocalName("Robot6");
+        
+        nusService.addCharacteristic(rxChar);
+        nusService.addCharacteristic(txChar);
+        BLE.addService(nusService);
+        
+        BLE.setAdvertisedService(nusService); // CRUCIAL pour la visibilité
+        BLE.advertise();
+    }
+
     pinMode(ROUE_ARRIERE_GAUCHE_AVANCER, OUTPUT);
     pinMode(ROUE_ARRIERE_GAUCHE_RECULER, OUTPUT);
     pinMode(ROUE_ARRIERE_GAUCHE_PWM, OUTPUT);
@@ -164,5 +219,78 @@ void setup() {
     pinMode(ROUE_AVANT_DROITE_PWM, OUTPUT);
 }
 
+void traiterCommande(char c, struct Actions& actions) {
+    constexpr int8_t testVitesse = 127;
+
+    switch (c) {
+        case 'w':
+        case 'W': {
+            actions = Actions(testVitesse, 0, 0);
+            Serial.println(F("[avant]"));
+        } break;
+
+        case 's':
+        case 'S': {
+            actions = Actions(-(testVitesse), 0, 0);
+            Serial.println(F("[recul]"));
+        } break;
+
+        case 'a':
+        case 'A': {
+            actions = Actions(0, static_cast<int8_t>(-testVitesse), 0);
+            Serial.println(F("[rot. gauche]"));
+        } break;
+
+        case 'd':
+        case 'D': {
+            actions = Actions(0, testVitesse, 0);
+            Serial.println(F("[rot. droite]"));
+        } break;
+
+        case 'q':
+        case 'Q': {
+            actions = Actions(0, 0, static_cast<int8_t>(-testVitesse));
+            Serial.println(F("[lat. gauche]"));
+        } break;
+
+        case 'e':
+        case 'E': {
+            actions = Actions(0, 0, testVitesse);
+            Serial.println(F("[lat. droite]"));
+        } break;
+
+        case 'x':
+        case 'X': {
+            actions = Actions(0, 0, 0);
+            Serial.println(F("[stop]"));
+        } break;
+
+        case '\r':
+        case '\n': {
+             // ignorer retours de ligne
+        } break;
+
+        default: {
+            Serial.print(F("Commande inconnue: "));
+            Serial.println(c);
+        } break;
+    }
+}
+
+
 void loop() {
+    static Actions actions(0, 0, 0);
+    BLE.poll(); // Traite les événements BLE internes
+
+    if (rxChar.written()) {
+        int len = rxChar.valueLength();
+        const uint8_t* val = rxChar.value();
+        
+        for (int i = 0; i < len; i++) {
+            char c = (char)val[i];
+            traiterCommande(c, actions); // ex: 'F' pour Forward, 'S' pour Stop
+        }
+    }
+
+    gererMouvement(actions);
 }
